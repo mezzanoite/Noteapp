abbffb1284d2d066afee53670c378d942529dc3f
logonrm <logonrm@fiap.com.br>
2018-03-03 13:45:23 -0300

    Salavando2 ...

--- app/src/main/java/br/com/mezzanotte/noteapp/MainActivity.kt
+++ app/src/main/java/br/com/mezzanotte/noteapp/MainActivity.kt
@@ -4,6 +4,18 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.PopupMenu
+import android.widget.Toast
+import java.nio.file.Files.delete
+import java.nio.file.Files.delete
+import android.R.id.edit
+import android.util.Log
+import android.widget.AdapterView.AdapterContextMenuInfo
+
+
+
+
 
 
 
@@ -19,4 +31,19 @@ class MainActivity : AppCompatActivity() {
         inflater.inflate(R.menu.menu_principal, menu)
         return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.pesquisar -> {
+                Toast.makeText(this, "Pesquisando ...", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.salvar -> {
+                Toast.makeText(this, "Salvando ...", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> return super.onContextItemSelected(item)
+        }
+    }
+
 }
