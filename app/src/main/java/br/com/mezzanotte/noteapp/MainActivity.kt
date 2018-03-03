e7978ea3d11f3cad3e6bd822c7bdae81dae478ed
logonrm <logonrm@fiap.com.br>
2018-03-03 10:13:29 -0300

    Salvando ...

--- app/src/main/java/br/com/mezzanotte/noteapp/MainActivity.kt
+++ app/src/main/java/br/com/mezzanotte/noteapp/MainActivity.kt
@@ -2,11 +2,21 @@ package br.com.mezzanotte.noteapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_principal, menu)
+        return true
     }
 }
